a53c113a8ed6296a5ecd44ca165a51877b404075
thiagola92 <thiagola92@gmail.com>
2019-04-12 20:02:16 -0300

    tolowercase

--- Testes/TrabLab001/src/example/MySignature.java
+++ Testes/TrabLab001/src/example/MySignature.java
@@ -11,7 +11,7 @@ public class MySignature {
 	private Cipher cipher;
 	
 	protected MySignature(String algorithm) throws Exception {
-		String[] algorithms = algorithm.split("with");
+		String[] algorithms = algorithm.toLowerCase().split("with");
 		
 		if(algorithms.length != 2) {
 			System.err.print("\nYou need to inform the algorithm from Digest and Asymetric Key Cypher, example: \"MD5WithRSA\"");
